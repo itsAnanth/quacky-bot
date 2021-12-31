000fa7d3fed7ed5080b47c31e40be7b00f347a5b
EJBEAN2op <danonymousarmy@gmail.com>
2021-12-31 18:25:18 +0530

    feat: util functions

--- modules/Formatter.js
+++ modules/Formatter.js
@@ -5,7 +5,7 @@ class _Formatter {
     static warns(data) {
         if (data.length == 0) return ['No Case Logs Found'];
         const formatted = data.map((v, i) => {
-            return `**Case: ${i}**\n**Moderator:** <@${v.author}>\n**Reason:** ${v.reason}\n**Time:** ${Formatters.time(v.time)}`;
+            return `**Case: ${i + 1}**\n**Moderator:** <@${v.author}>\n**Reason:** ${v.reason}\n**Time:** ${Formatters.time(new Date(v.time))}`;
         });
         return formatted;
     }
--- modules/utils.js
+++ modules/utils.js
@@ -1,10 +1,25 @@
-import { Formatters, Message } from 'discord.js';
+import { Formatters, Message, MessageEmbed, User } from 'discord.js';
 
 function init() {
+    User.prototype.sendEmbed = async function({ user, color, description, footer }) {
+        const embed = new MessageEmbed()
+            .setAuthor({ name: user.username, iconURL: user.iconURL })
+            .setDescription(description)
+            .setColor(color ? color : 'GOLD')
+            .setFooter({ text: footer ? footer : '' });
+        await this.send({ embeds: [embed], failIfNotExists: false }).catch(() => {});
+    };
+
+    Message.prototype.getMember = async function(string) {
+        const member = await this.guild.members.cache.get(string.replace(/\D/g, ''));
+        return member ? member : null;
+    };
+
     Message.prototype.getUser = async function(string) {
         const user = await this.client.users.fetch(string.replace(/\D/g, '')).catch(() => {});
         return user ? user : null;
     };
+
 
     Message.prototype.disableComponents = function() {
         const newRows = this.components.map(row => {
@@ -12,6 +27,24 @@ function init() {
             return row;
         });
         return this.edit({ components: newRows });
+    };
+
+    Message.prototype.sendEmbed = function({ user, color, description, footer }) {
+        const embed = new MessageEmbed()
+            .setAuthor({ name: user.username, iconURL: user.iconURL })
+            .setDescription(description)
+            .setColor(color ? color : 'GOLD')
+            .setFooter({ text: footer ? footer : '' });
+        this.channel.send({ embeds: [embed], failIfNotExists: false }).catch(() => {});
+    };
+
+    Message.prototype.replyEmbed = function({ user, color, description, footer }) {
+        const embed = new MessageEmbed()
+            .setAuthor({ name: user.username, iconURL: user.iconURL })
+            .setDescription(description)
+            .setColor(color ? color : 'GOLD')
+            .setFooter({ text: footer ? footer : '' });
+        this.reply({ embeds: [embed], failIfNotExists: false }).catch(() => {});
     };
 
     String.prototype.dBold = function() {
