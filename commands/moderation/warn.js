20aded2f1c2fc8ee1bc28adf1247ced6aa5f6dac
EJBEAN2op <danonymousarmy@gmail.com>
2021-12-31 18:25:58 +0530

    feat: warn & warnlist

--- commands/moderation/warn.js
+++ commands/moderation/warn.js
@@ -1,7 +1,6 @@
-import { core, roles, devs } from '../../data/index.js';
+import { core } from '../../data/index.js';
 import { createEmbed } from '../../modules/messageUtils.js';
 import db from '../../modules/db/main.js';
-import { Formatters } from 'discord.js';
 
 export default {
     name: 'warn',
@@ -11,20 +10,18 @@ export default {
     excpectedArgs: `${core.prefix} warn [ID / @user] (reason)`,
     useOnly: { permissions: ['BAN_MEMBERS'], roles: ['b'] },
     execute: async(message, args) => {
-        message.channel.send(Formatters.time(Date.now()))
-        // if (!args[0]) return message.reply(createEmbed(message.author, 'RED', 'Missing user'));
-        // if (!args[1]) return message.reply(createEmbed(message.author, 'RED', 'Missing Reason'));
-        // const user = await message.getUser(args[0]);
-        // if (!user) return message.reply('Unknown user');
-        // const reason = args[1] ? args.slice(1, args.length).join(' ') : 'No Reason Provided';
-        // const warnObj = {
-        //     reason: reason,
-        //     time: Date.now(),
-        //     author: message.author.id
-        // };
-        // await db.utils.warn(user.id, warnObj);
-        // console.log(user);
-        // message.reply(createEmbed(message.author, 'RED', `Warned user ${user.tag} with reason : \n\`\`\`${reason}\`\`\``));
-        // console.log(await db.utils.getWarns(user.id));
+        if (!args[0]) return message.reply(createEmbed(message.author, 'RED', 'Missing user'));
+        if (!args[1]) return message.reply(createEmbed(message.author, 'RED', 'Missing Reason'));
+        const user = await message.getMember(args[0]);
+        if (!user) return message.replyEmbed({ user: message.author, color: 'RED', description: 'Unknown User' });
+        const reason = args[1] ? args.slice(1, args.length).join(' ') : 'No Reason Provided';
+        const warnObj = {
+            reason: reason,
+            time: Date.now(),
+            author: message.author.id
+        };
+        const msgObj = { user: message.author, color: 'GREEN', description: `Warned user ${user.user.tag} with reason\n\`\`\`${reason}\`\`\``, footer: 'Logged response' };
+        await db.utils.warn(user.id, warnObj);
+        message.replyEmbed(msgObj);
     }
 };
---
+++ commands/moderation/warnList.js
@@ -0,0 +1,37 @@
+import { core } from '../../data/index.js';
+import { createEmbed } from '../../modules/messageUtils.js';
+import db from '../../modules/db/main.js';
+import { MessageEmbed } from 'discord.js';
+import Paginator from '../../modules/Paginator.js';
+import _Formatter from '../../modules/Formatter.js';
+export default {
+    name: 'warnlist',
+    aliases: ['warns'],
+    cooldown: 0,
+    descriptions: 'Warns a user with reason, if any',
+    excpectedArgs: `${core.prefix} warn [ID / @user] (reason)`,
+    useOnly: { permissions: [], roles: ['b'], ids: [] },
+    execute: async(message, args, bot) => {
+        if (!args[0]) return message.reply(createEmbed(message.author, 'RED', 'Missing user'));
+        const user = await message.getMember(args[0]);
+        if (!user) return message.reply('Unknown user');
+        const raw = await db.utils.getWarns(user.id);
+
+        const data = _Formatter.warns(raw);
+        console.log(data);
+
+        const lastPage = Math.ceil(data.length / core['page-break']);
+        const options = { author: message.author, current: 1, maxValues: data.length, max: lastPage, count: core['page-break'] };
+        const paginator = new Paginator(bot, message.channel, options, async(i, dat) => {
+            const final = [...data].slice(i, i + core['page-break']);
+            console.log(i);
+            return { embeds: [new MessageEmbed()
+                .setAuthor({ name: `Total Case Logs: ${raw.length}` })
+                .setFooter(`${dat.page} out of ${lastPage == 0 ? 1 : lastPage}`)
+                .setTitle(`${user.username}'s Warnlist`)
+                .setColor(core.embed)
+                .setDescription(final.join('\n\u200b\n'))] }; // return embed
+        });
+        await paginator.start();
+    }
+};
