2ab5cfaf4d36beeeaa92f5f4b67e66fd7ef1eb4a
EJBEAN2op <danonymousarmy@gmail.com>
2022-01-01 15:28:35 +0530

    feat: db commands

--- commands/dev/resetDB.js
+++ commands/dev/resetDB.js
@@ -1,15 +1,16 @@
 import { core, devs } from '../../data/index.js';
-import warnDB from '../../modules/db/main.js';
-
+import mainDB from '../../modules/db/main.js';
+import serverDB from '../../modules/db/server.js';
 export default {
-    name: 'resetDB',
+    name: 'resetdb',
     aliases: ['reset'],
     cooldown: 0,
     descriptions: 'Warns a user with reason, if any',
     excpectedArgs: `${core.prefix} warn [ID / @user] (reason)`,
     useOnly: { permissions: [], roles: [], ids: devs },
     execute: async(message) => {
-        warnDB.clear();
-        message.sendEmbed({ user: message.author, color: null, description: 'Successfully earsed database' });
+        mainDB.clear();
+        serverDB.clear();
+        message.sendEmbed(null, null, 'Successfully earsed database');
     }
 };
