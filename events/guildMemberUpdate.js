3243a50942573efaf266a2fb94febceec2ab5b59
EJBEAN2op <danonymousarmy@gmail.com>
2022-01-03 01:07:20 +0530

    feat: userupdate event

--- events/guildMemberUpdate.js
+++ events/guildMemberUpdate.js
@@ -4,32 +4,54 @@ import db from '../modules/db/server.js';
 export default {
     name: 'guildMemberUpdate',
     execute: async(bot, oldMember, newMember) => {
-        const Lchannels = await db.utils.log_channels();
-        const mId = Lchannels.roleupdate;
-        if (!mId) return;
-        const mC = bot.channels.resolve(mId);
-        if (!mC) return;
-        if (oldMember.roles.cache.size != newMember.roles.cache.size) {
-            const type = oldMember.roles.cache.size > newMember.roles.cache.size ? 'removed' : 'added';
-
-            const fetchedLogs = await oldMember.guild.fetchAuditLogs({
-                limit: 1,
-                type: 'MEMBER_ROLE_UPDATE',
-            });
-
-            const roleAddLog = fetchedLogs.entries.first();
-            if (!roleAddLog) return;
-            const { executor, target, changes } = roleAddLog;
-            const embed = new MessageEmbed()
-                .setAuthor({ name: newMember.user.username, iconURL: newMember.user.avatarURL() })
-                .setTitle(`Role ${type}`)
-                .setColor('BLUE')
-                .setDescription(`**Role:** ${Formatters.roleMention(changes[0].new[0].id)}
-                \n**Author:** <@${executor.id}>`)
-                .setFooter({ text: `User ID: ${target.id}` })
-                .setTimestamp();
-            await mC.send({ embeds: [embed] }).catch(console.error);
-        }
+        if (oldMember.roles.cache.size != newMember.roles.cache.size)
+            handleRoleUpdate(bot, oldMember, newMember);
+        else if (oldMember.nickname != newMember.nickname)
+            handleUserUpdate(bot, oldMember, newMember);
     }
 };
+
+async function handleUserUpdate(bot, oldMember, newMember) {
+    const Lchannels = await db.utils.log_channels();
+    const mId = Lchannels.userupdate;
+    if (!mId) return;
+    const mC = bot.channels.resolve(mId);
+    if (!mC) return;
+
+    const embed = new MessageEmbed()
+        .setAuthor({ name: newMember.user.username, iconURL: newMember.user.avatarURL() })
+        .setTitle('Nickname Changed')
+        .setColor('BLUE')
+        .setDescription(`**Before:** ${Formatters.codeBlock(oldMember.nickname)}\n**After:** ${Formatters.codeBlock(newMember.nickname)}`)
+        .setFooter({ text: `User ID: ${newMember.id}` })
+        .setTimestamp();
+    await mC.send({ embeds: [embed] }).catch(console.error);
+}
+
+async function handleRoleUpdate(bot, oldMember, newMember) {
+    const Lchannels = await db.utils.log_channels();
+    const mId = Lchannels.roleupdate;
+    if (!mId) return;
+    const mC = bot.channels.resolve(mId);
+    if (!mC) return;
+    const type = oldMember.roles.cache.size > newMember.roles.cache.size ? 'removed' : 'added';
+
+    const fetchedLogs = await oldMember.guild.fetchAuditLogs({
+        limit: 1,
+        type: 'MEMBER_ROLE_UPDATE',
+    });
+
+    const roleAddLog = fetchedLogs.entries.first();
+    if (!roleAddLog) return;
+    const { executor, target, changes } = roleAddLog;
+    const embed = new MessageEmbed()
+        .setAuthor({ name: newMember.user.username, iconURL: newMember.user.avatarURL() })
+        .setTitle(`Role ${type}`)
+        .setColor('BLUE')
+        .setDescription(`**Role:** ${Formatters.roleMention(changes[0].new[0].id)}
+        \n**Author:** <@${executor.id}>`)
+        .setFooter({ text: `User ID: ${target.id}` })
+        .setTimestamp();
+    await mC.send({ embeds: [embed] }).catch(console.error);
+}
 
