8f2e5d3a03b289fbd9ad040e6e72a66ee7279d68
EJBEAN2op <danonymousarmy@gmail.com>
2022-01-02 16:06:19 +0530

    feat: replyembed

--- commands/filter/banword.js
+++ commands/filter/banword.js
@@ -1,5 +1,4 @@
 import { core } from '../../data/index.js';
-import { createEmbed } from '../../modules/messageUtils.js';
 import db from '../../modules/db/server.js';
 import { Permissions } from 'discord.js';
 
@@ -8,11 +7,11 @@ export default {
     aliases: ['banword'],
     cooldown: 0,
     descriptions: 'Warns a user with reason, if any',
-    excpectedArgs: `${core.prefix} warn [ID / @user] (reason)`,
+    excpectedArgs: `${core.prefix}banword [word]`,
     useOnly: { permissions: [Permissions.FLAGS.MANAGE_MESSAGES], roles: [] },
     required: { permissions: [] },
     execute: async function(message, args) {
-        if (!args[0]) return message.reply(createEmbed(message.author, 'RED', 'Missing argument | `word to blacklist`'));
+        if (!args[0]) return message.replyEmbed(null, 'RED', `Missing argument\n\`${this.excpectedArgs}\``);
         const word = args.slice(0, args.length).join('').toLowerCase();
         const filter = await db.utils.filter.get();
         if (filter.find(x => x == word)) return message.replyEmbed(null, 'RED', `The word \`${word}\` already exists in filter`);
