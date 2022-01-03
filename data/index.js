65f2fbfc79579278664f6147c3d6c8ea5287cbbc
EJBEAN2op <danonymousarmy@gmail.com>
2022-01-04 00:30:57 +0530

    chore: metadata update

--- data/index.js
+++ data/index.js
@@ -2,5 +2,5 @@ import devs from './JSON/devs.json';
 import core from './JSON/core.json';
 import id from './JSON/id.json';
 import roles from './JSON/roles.json';
-
-export { id, devs, core, roles };
+import emotes from './JSON/emotes.json';
+export { id, devs, core, roles, emotes };
