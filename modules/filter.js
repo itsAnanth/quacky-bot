3e07f2a86794f3a22716a586f760ce29ff966dce
EJBEAN2op <danonymousarmy@gmail.com>
2022-01-04 00:33:18 +0530

    feat: better filter

--- modules/filter.js
+++ modules/filter.js
@@ -1,14 +1,22 @@
 import { Permissions } from 'discord.js';
 import db from './db/server.js';
-import { userHasPermission } from './evalCommand.js';
+import { isStaff } from './evalCommand.js';
+import warn from '../commands/moderation/warn.js';
+import kick from '../commands/moderation/kick.js';
 export default {
     name: 'filter',
     bypass: { permissions: [Permissions.FLAGS.MANAGE_MESSAGES] },
+    staff: ['admin', 'mod'],
     execute: async function(message) {
-        if (userHasPermission(message, this.bypass.permissions)) return false;
+        if (await isStaff(this, message)) return false;
+        // if (userHasPermission(message, this.bypass.permissions)) return false;
+        if (await checkMentions(message)) return true;
+
         const filter = await db.utils.filter.get();
         const str = message.content.toLowerCase();
         const filtered = str.replaceAll(/\s/g, '');
+
+        console.log(filter);
 
         const roleW = await db.utils.filter.getWhitelistRole();
 
@@ -16,11 +24,11 @@ export default {
 
         if (idx != -1) {
             for (let i = 0; i < roleW[idx].words.length; i++)
-                if (filtered.includes(roleW[idx].words[i])) return false;
+                if (filtered.find(x => x[0].includes(roleW[idx].words[i]))) return false;
         }
         // if (filtered.length != copy.replace(/[^\x00-\x7F]/g, '')) return true;
-        for (let i = 0; i < filter.length; i++)
-            if (filtered.includes(filter[i])) return true;
+        // for (let i = 0; i < filter.length; i++)
+        if (filter.find(x => x[0].includes(filtered))) return true;
 
         return false;
 
@@ -31,3 +39,17 @@ export default {
         }
     }
 };
+
+async function checkMentions(message) {
+    const mentionsSize = message.content.match(/<@(&|!)?\d+>/gm)?.length || 0;
+    const reason = 'Too many mentions in one message'.split(' ');
+    if (mentionsSize >= 3 && mentionsSize < 10) {
+        await warn.execute(message, [message.author.id, ...reason]);
+        return true;
+    } else if (mentionsSize >= 10) {
+        await kick.execute(message, [message.author.id, ...reason]);
+        return true;
+    }
+    return false;
+}
+
