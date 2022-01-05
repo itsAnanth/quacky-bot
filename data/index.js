64b7842f088c4f709830f19fa6bafd75a71a07a9
EJBEAN2op <danonymousarmy@gmail.com>
2022-01-05 21:06:41 +0530

    default module

--- data/index.js
+++ data/index.js
@@ -3,4 +3,5 @@ import core from './JSON/core.json';
 import id from './JSON/id.json';
 import roles from './JSON/roles.json';
 import emotes from './JSON/emotes.json';
+export default { id, devs, core, roles, emotes };
 export { id, devs, core, roles, emotes };
