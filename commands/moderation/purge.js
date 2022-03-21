0f10f8e407e0e3c15a9ab399c8359d982be7e92e
EJBEAN2op <danonymousarmy@gmail.com>
2022-03-21 22:25:47 +0530

    feat(*): user only purge

--- commands/moderation/purge.js
+++ commands/moderation/purge.js
@@ -7,7 +7,7 @@ export default {
     aliases: ['clear'],
     cooldown: 0,
     descriptions: 'Deletes a specific amount of messages in a channel (100 max limit)',
-    excpectedArgs: `${core.prefix}purge [number]`,
+    excpectedArgs: `${core.prefix}purge [number] [userID/@user optional]`,
     useOnly: { permissions: [], roles: [] },
     required: { permissions: [Permissions.FLAGS.MANAGE_MESSAGES] },
     staff: ['admin'],
@@ -16,7 +16,21 @@ export default {
         const count = parseInt(args[0]);
         if (isNaN(count)) return message.replyEmbed(null, 'RED', 'Invalid argument | Expected a number');
         if (count < 0 || count > 100) return message.replyEmbed(null, 'RED', 'Messages count must be greater than 0 and less than 100');
-        message.channel.bulkDelete(count);
-        message.sendEmbed(null, 'GREEN', `Purged ${args[0]} messages`);
+
+        if (!args[1]) {
+            message.channel.bulkDelete(count);
+            message.sendEmbed(null, 'GREEN', `Purged ${args[0]} messages`);
+        } else {
+            const user = await message.getMember(args[1]);
+            if (!user) return message.replyEmbed(null, 'RED', 'Unknown User');
+
+            const messages = [...(await message.channel.messages.fetch({ limit: 100 })).values()];
+            const msgs = messages.filter(x => x.author.id == user.id).slice(0, count + 1);
+
+            for (let i = 0; i < msgs.length; i++)
+                await msgs[i].delete().catch(console.error);
+
+            await message.sendEmbed(null, 'GREEN', `Deleted ${msgs.length} messages from ${user.user.tag}`);
+        }
     }
 };
