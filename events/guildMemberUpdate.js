540b3d22ee1bd1770d956413ce5155b1a1bada80
EJBEAN2op <danonymousarmy@gmail.com>
2022-03-24 22:33:27 +0530

    feat(*): screening welcome

--- events/guildMemberUpdate.js
+++ events/guildMemberUpdate.js
@@ -4,14 +4,31 @@ import db from '../modules/db/server.js';
 export default {
     name: 'guildMemberUpdate',
     execute: async(bot, oldMember, newMember) => {
+        if (oldMember.pending && !newMember.pending)
+            handleScreening(bot, oldMember, newMember);
+
         if (oldMember.roles.cache.size != newMember.roles.cache.size) {
-            handleBooster(bot, oldMember, newMember);
+            // handleBooster(bot, oldMember, newMember);
             handleRoleUpdate(bot, oldMember, newMember);
         } else if (oldMember.nickname != newMember.nickname)
             handleUserUpdate(bot, oldMember, newMember);
     }
 };
 
+
+async function handleScreening(bot, o, n) {
+    const channel = (o.guild.channels || n.guild.channels).cache.get('927953865838579785');
+    if (!channel) return;
+    const embed = new MessageEmbed()
+        .setColor('#FF8600')
+        .setAuthor({ name: `Welcome ${o.user.tag}`, iconURL: 'https://images-ext-2.discordapp.net/external/mxgXDBnPaVN6O7yiPmS-H4f0ss1aiEQo9NnGO_A1TRc/https/media.discordapp.net/attachments/714445203318112256/956596676624252928/hello.gif' })
+        .setThumbnail(n.user.displayAvatarURL({ dynamic: true }))
+        .setDescription(`To fully understand the server, we strongly suggest recommend that you: \n\n\`1.\` Read the ${Formatters.channelMention('817672264292106240')}\n\`2.\` Select your roles in ${Formatters.channelMention('722708128948420649')}\n\`3.\` Introduce yourself ${Formatters.channelMention('955343973638082561')}`)
+        .setFooter({ text: 'Welcome them to The Quack Pack!', iconURL: 'https://media.discordapp.net/attachments/714445203318112256/956597062844166204/893165570015b53aad1a6999d13ea7d8.png' });
+    await channel.send({ content: `${Formatters.userMention(o.id)}`, embeds: [embed] });
+}
+
+// eslint-disable-next-line no-unused-vars
 async function handleBooster(bot, oldMember, newMember) {
     const Lchannels = await db.utils.log_channels();
     const mId = Lchannels.booster;
